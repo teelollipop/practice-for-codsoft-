d0333bb86626f77fadd01c6f7fbe9a09411b3f65
teelollipop <129333013+teelollipop@users.noreply.github.com>
2023-09-12 23:09:51 +0000

    Making the code more efficient

--- Task 1/Chatbot.py
+++ Task 1/Chatbot.py
@@ -11,13 +11,27 @@ rules = {
 
 #Create chatbot response function
 #Import the regular expresions module
-import re
+#import re
 
-def chatbot_response(user_input):
-    for pattern, response in rules.items():
-        if re.search(pattern, user_input, re.IGNORECASE):
-            return response
-    return "I'm sorry, I didn't understand that."
+#def chatbot_response(user_input):
+  #  for pattern, response in rules.items():
+     #   if re.search(pattern, user_input, re.IGNORECASE):
+           # return response
+    #return "I'm sorry, I didn't understand that."
+
+#Create a no response function
+
+def no_response(user_input):
+  if user_input not in responses:
+    return "I'm sorry, I don't know how to respond to that."
+  else:
+    return responses[user_input][0]
+
+print(no_response("Hello"))
+print(no_response("How are you?"))
+print(no_response("What's your name?"))
+print(no_response("I'm fine"))
+
 
 #Create a loop to take user input and provide responses until the user decides to exit
 while True:
--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-#Install the regex library for pattern recognition using pip install regex
+#Create chatbot response function
+#Import the regular expresions module
 
-#Create a dictionary of user inputs and pattern response pairs
+import re
+
+def chatbot_response(user_input, rules):
+    response = None
+    for pattern in rules:
+        pattern_with_wildcard = str(pattern) + '*'     
+#Add '*' as a wildcard to the pattern
+      
+        if re.search(pattern_with_wildcard, user_input, re.IGNORECASE):
+            response = rules[pattern]
+            break
+          
+    if response is None:
+      #Check if no match was found
+      return "I'm sorry, I didn't understand that."
+      
+    return response 
+
+#Define rules by creating a dictionary of user inputs and pattern response pairs
 
 rules = {
     r'hello|hi|hey': 'Hello! How can I assist you?',
     r'bye|goodbye': 'Goodbye! Have an amazing day!',
     r'how are you': 'I am just a chatbot, but thanks for asking!',
     r'what is your name': 'My name is ChatTAI, an AI system created by Treasure Abaa',
-    r'what can you do': 'I provide text based responses based on user inputs',
-    r'help|?': 'I can provide basic information. Try asking a question.'
-}
+    r'what can you do': 'I can provide text based responses based on user inputs',
+    r'help|?': 'I can provide basic information. Try asking a question.', }
 
-#Create chatbot response function
-
-#Import the regular expresions module
-
-import re
-
-def chatbot_response(user_input):
-    for pattern, response in rules.items():
-        if re.search(pattern, user_input, re.IGNORECASE):
-            return response
-    return "I'm sorry, I didn't understand that."
-
-#Create a loop to take user input and provide responses until the user decides to exit
+#Create main loop for interacting with the chatbot 
+#This loop takes user input and provide responses until the user decides to exit
 
 while True:
     user_input = input("You: ")
-    if user_input.lower() == 'exit':
-        break
-    response = chatbot_response(user_input)
+    if user_input.strip() == '':
+        print("Please enter a valid input.")
+        continue
+
+    response = chatbot_response(user_input, rules)
     print("Chatbot:", response)
